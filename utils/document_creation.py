79e7d2ae97719a38fc99943c0c7ce74025066ba7
pChitral <chitral.patil@vit.edu.in>
2023-08-10 17:31:27 -0700

    left align added

--- utils/document_creation.py
+++ utils/document_creation.py
@@ -2,7 +2,9 @@ from docx import Document
 
 def create_word_document(file_name, response_list):
     doc = Document()
-    for i, generated_text in enumerate(response_list):
-        doc.add_paragraph(generated_text)
-
+    
+    for generated_text in response_list:
+        paragraph = doc.add_paragraph(generated_text)
+        paragraph.alignment = 0  # Left alignment
+    
     doc.save(file_name + ".docx")
